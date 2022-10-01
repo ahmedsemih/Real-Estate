bb5ba249db71a0e1c2b815d2858909c1424560e9
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2022-10-01 17:17:12 +0300

    Base structure created

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config();
+
+const searchRoutes = require("./routes/searches");
+const authRoutes = require("./routes/auths");
+const accountRoutes = require("./routes/accounts");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// MIDDLEWARES
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTES
+app.use("/", searchRoutes);
+app.use("/auth", authRoutes);
+app.use("/account", accountRoutes);
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**.ejs","./views/layouts/**.ejs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
